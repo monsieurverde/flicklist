8ac09995555d90059e18c578d30b5c094a9b550b
monsieurverde <domverdemgmt@gmail.com>
2019-09-17 19:58:01 -0400

    converted more forms into templates using jinja

--- main.py
+++ main.py
@@ -1,99 +1,74 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, render_template
+import os
+import jinja2
+import cgi
 
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 app = Flask(__name__)
+
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-page_header = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>FlickList</title>
-    </head>
-    <body>
-        <h1>FlickList</h1>
-"""
+# a list of movies that nobody should have to watch
+terrible_movies = [
+    "Gigli",
+    "Star Wars Episode 1: Attack of the Clones",
+    "Paul Blart: Mall Cop 2",
+    "Nine Lives",
+    "Starship Troopers"
+]
 
-page_footer = """
-    </body>
-</html>
-"""
+def get_current_watchlist():
+    # returns user's current watchlist--hard coded for now
+    #return []
+    return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
 
-# a form for adding new movies
-add_form = """
-    <form action="/add" method="post">
-        <label for="new-movie  ">
-            I want to add
-            <input type="text" id="new-movie" name="new-movie"/>
-            to my watchlist.
-        </label>
-        <input type="submit" value="Add It"/>
-    </form>
-"""
 
-# TODO:
-# Create the HTML for the form below so the user can check off a movie from their list 
-# when they've watched it.
-# Name the action for the form '/crossoff' and make its method 'post'.
-
-# a form for crossing off watched movies
-crossoff_form = """
-    <form action="/crossed-off" method="post">
-            <label for="new-movie">
-                I want to cross-off
-                <select name="new-movie">
-                    <option value="Star Wars">Star Wars</option>
-                    <option value="Aliens">Aliens</option>
-                    <option value="Serenity">Serenity</option>
-                    <option value="Predator">Predator</option>
-                </select>
-                from my watchlist.
-            </label>
-            <input type="submit" value="Cross Off"/>
-        </form>
-
-"""
-
-# TODO:
-# Finish filling in the function below so that the user will see a message like:
-# "Star Wars has been crossed off your watchlist".
-# And create a route above the function definition to receive and handle the request from 
-# your crossoff_form.
-@app.route("/crossed-off", methods=['POST'])
+@app.route("/crossoff", methods=['POST'])
 def crossoff_movie():
-    new_movie = request.form['new-movie']
-    
-    new_movie_element = "<strong>" + new_movie + "</strong>"
-    sentence = new_movie_element + " has been crossed-off your Watchlist!"
-    content = page_header + "<p>" + sentence + "</p>" + page_footer
+    crossed_off_movie = request.form['crossed-off-movie']
 
-    return content
+    if crossed_off_movie not in get_current_watchlist():
+        # the user tried to cross off a movie that isn't in their list,
+        # so we redirect back to the front page and tell them what went wrong
+        error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
 
+        # redirect to homepage, and include error as a query parameter in the URL
+        return redirect("/?error=" + error)
 
-# TODO:
-# modify the crossoff_form above to use a dropdown (<select>) instead of
-# an input text field (<input type="text"/>)
+    # if we didn't redirect by now, then all is well
+    return render_template('crossoff.html', crossed_off_movie=crossed_off_movie)
 
 @app.route("/add", methods=['POST'])
 def add_movie():
+    # look inside the request to figure out what the user typed
     new_movie = request.form['new-movie']
 
-    # build response content
-    new_movie_element = "<strong>" + new_movie + "</strong>"
-    sentence = new_movie_element + " has been added to your Watchlist!"
-    content = page_header + "<p>" + sentence + "</p>" + page_footer
+    # if the user typed nothing at all, redirect and tell them the error
+    if (not new_movie) or (new_movie.strip() == ""):
+        error = "Please specify the movie you want to add."
+        return redirect("/?error=" + error)
 
-    return content
+    # if the user wants to add a terrible movie, redirect and tell them the error
+    if new_movie in terrible_movies:
+        error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
+        return redirect("/?error=" + error)
+
+    # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+    new_movie_escaped = cgi.escape(new_movie, quote=True)
+    add_confirmation = jinja_env.get_template('add_confirmation.html')
+    
+    # TODO:
+    # Create a template called add-confirmation.html inside your /templates directory
+    # Use that template to render the confirmation message instead of this temporary message below
+    return render_template('add_confirmation.html', new_movie=new_movie)
 
 
 @app.route("/")
 def index():
-    edit_header = "<h2>Edit My Watchlist</h2>"
-
-    # build the response string
-    content = page_header + edit_header + crossoff_form + page_footer
-
-    return content
-
+    encoded_error = request.args.get("error")
+    return render_template('edit.html', watchlist=get_current_watchlist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 app.run()
